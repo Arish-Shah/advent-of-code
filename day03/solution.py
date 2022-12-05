b8e62453c2d5615e8264d27d1c95cd4593780de6
Arish-Shah <arish.r.shah@gmail.com>
2022-12-05 12:37:34 +0100

    janky way without intersection :alien:

--- day03/solution.py
+++ day03/solution.py
@@ -1,25 +1,33 @@
 lines = open("./input").read().strip().split("\n")
 
-part1 = 0
-part2 = 0
+def letter_sum(letters):
+    s = 0
+    for letter in letters:
+        sub = 38 if letter.isupper() else 96
+        s += (ord(letter) - sub)
+    return s
+
+common1: list[str] = []
+common2: list[str] = []
 
 for line in lines:
-    first = [line[n] for n in range(0, int(len(line) / 2))]
-    second = [line[n] for n in range(int(len(line) / 2), len(line))]
-    for inter in list(set(first).intersection(second)):
-        if inter.isupper():
-            part1 += (ord(inter) - 38)
-        else:
-            part1 += (ord(inter) - 96)
+    first = line[0:int(len(line) / 2)]
+    second = line[int(len(line) / 2):len(line)]
+    s = set()
+    for letter in first:
+        if letter in second:
+            s.add(letter)
+    common1.extend(s)
+part1 = letter_sum(common1)
 print(part1)
 
 for i in range(0, len(lines), 3):
-    group = lines[i:i + 3]
-    for inter in list(set(group[0]).intersection(group[1]).intersection(group[2])):
-        if inter.isupper():
-            part2 += (ord(inter) - 38)
-        else:
-            part2 += (ord(inter) - 96)
+    grp = lines[i:i + 3]
+    s = set()
+    for letter in grp[0]:
+        if letter in grp[1] and letter in grp[2]:
+            s.add(letter)
+    common2.extend(s)
 
+part2 = letter_sum(common2)
 print(part2)
-
