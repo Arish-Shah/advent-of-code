39b13b3601d4b5a6d55b2118ce77c1b58e1fe6e1
Arish-Shah <arish.r.shah@gmail.com>
2022-12-08 11:51:59 +0100

    finish day 8

---
+++ day08/solution.py
@@ -0,0 +1,48 @@
+lines = open("input", "r").readlines()
+
+grid = []
+part1 = 0
+part2 = 0
+
+for line in lines:
+    line = line.replace("\n", "")
+    grid.append([])
+    for n in line:
+        grid[-1].append(int(n))
+
+part1 += ((len(grid) * 2) + (len(grid[0]) * 2) - 4)
+
+for i in range(1, len(grid) - 1):
+    for j in range(1, len(grid[i]) - 1):
+        current = grid[i][j]
+        t = b = l = r = False
+        st = sb = sl = sr = 0
+
+        for k in range(i - 1, -1, -1):
+            st += 1
+            if grid[k][j] >= current:
+                t = True
+                break
+        for k in range(i + 1, len(grid)):
+            sb += 1
+            if grid[k][j] >= current:
+                b = True
+                break
+        for k in range(j - 1, -1, -1):
+            sl += 1
+            if grid[i][k] >= current:
+                l = True
+                break
+        for k in range(j + 1, len(grid[i])):
+            sr += 1
+            if grid[i][k] >= current:
+                r = True
+                break
+        
+        part2 = max(part2, st * sb * sl * sr)
+
+        if not t or not b or not l or not r:
+            part1 += 1
+
+print(part1)
+print(part2)
