13b6d115427c44893dc91a9dfadde96524f4bd49
Arish-Shah <arish.r.shah@gmail.com>
2022-12-09 22:35:12 +0100

    shorter now

--- day09/solution.py
+++ day09/solution.py
@@ -1,65 +1,42 @@
 lines = open("example", "r").readlines()
 
+hx = hy = tx = ty = 0
+ss: list[list[tuple[int, int]]] = [[(0, 0)] for _ in range(10)]
 def get_next_hop(hy, hx, ty, tx):
     dy = hy - ty
     dx = hx - tx
 
     if dy == 2:
-        ty += 1
-        if abs(dx) == 1:
-            tx = hx
+        return (hy - 1, hx)
     elif dy == -2:
-        ty -= 1
-        if abs(dx) == 1:
-            tx = hx
+        return (hy + 1, hx)
     elif dx == 2:
-        tx += 1
-        if abs(dy) == 1:
-            ty = hy
+        return (hy, hx - 1)
     elif dx == -2:
-        tx -= 1
-        if abs(dy) == 1:
-            ty = hy
-    return ty, tx
-
-m = ["."]
-hx = hy = tx = ty = 0
-part1 = 0
+        return (hy, hx + 1)
+    return (ty, tx)
 
 for line in lines:
     d, n = line.split()
     n = int(n)
-    for i in range(n):
+    for _ in range(n):
         if d == "U":
-            hy -= 1
-        elif d == "D":
             hy += 1
-        elif d == "L":
-            hx -= 1
+        elif d == "D":
+            hy -= 1
         elif d == "R":
             hx += 1
+        elif d == "L":
+            hx -= 1
 
-        if hy == -1:
-            m.insert(0, "." * len(m[0]))
-            hy = 0
-            ty += 1
-        if hy == len(m):
-            m.append("." * len(m[0]))
-        if hx == -1:
-            for x in range(len(m)):
-                m[x] = "." + m[x]
-            hx = 0
-            tx += 1
-        if hx == len(m[0]):
-            for x in range(len(m)):
-                m[x] = m[x] + "."
+        ss[0].append((hy, hx))
 
-        ty, tx = get_next_hop(hy, hx, ty, tx)
-        m[ty] = m[ty][:tx] + "#" + m[ty][tx + 1:]
+        for i in range(1, 10):
+            head_y, head_x = ss[i - 1][-1]
+            tail_y, tail_x = ss[i][-1]
+            ss[i].append(get_next_hop(head_y, head_x, tail_y, tail_x))
+            if i == 5:
+                print(head_y, head_x, tail_y, tail_x)
 
-for row in m:
-    for col in row:
-        if col == "#":
-            part1 += 1
-
-print(part1)
+print(len(set(ss[1])))
+print(len(set(ss[9])))
