37feffe96512f5e652e332f68ced408770126645
Arish-Shah <arish.r.shah@gmail.com>
2022-12-09 23:52:57 +0100

    finish day 9, part 2 :disappointed_relieved:

--- day09/solution.py
+++ day09/solution.py
@@ -1,25 +1,35 @@
-lines = open("example", "r").readlines()
+lines = open("input", "r").readlines()
 
-hx = hy = tx = ty = 0
-ss: list[list[tuple[int, int]]] = [[(0, 0)] for _ in range(10)]
 def get_next_hop(hy, hx, ty, tx):
-    dy = hy - ty
-    dx = hx - tx
+    dy = ty - hy
+    dx = tx - hx
 
+    if dy == -2:
+        ty = hy - 1
+        if abs(dx) == 1:
+            tx = hx
     if dy == 2:
-        return (hy - 1, hx)
-    elif dy == -2:
-        return (hy + 1, hx)
-    elif dx == 2:
-        return (hy, hx - 1)
-    elif dx == -2:
-        return (hy, hx + 1)
+        ty = hy + 1
+        if abs(dx) == 1:
+            tx = hx
+    if dx == -2:
+        tx = hx - 1
+        if abs(dy) == 1:
+            ty = hy
+    if dx == 2:
+        tx = hx + 1
+        if abs(dy) == 1:
+            ty = hy
+
     return (ty, tx)
+
+hy = hx = ty = tx = 0
+ss: list[list[tuple[int, int]]] = [[(0, 0)] for _ in range(10)]
 
 for line in lines:
     d, n = line.split()
     n = int(n)
-    for _ in range(n):
+    for i in range(n):
         if d == "U":
             hy += 1
         elif d == "D":
@@ -35,8 +45,6 @@ for line in lines:
             head_y, head_x = ss[i - 1][-1]
             tail_y, tail_x = ss[i][-1]
             ss[i].append(get_next_hop(head_y, head_x, tail_y, tail_x))
-            if i == 5:
-                print(head_y, head_x, tail_y, tail_x)
 
-print(len(set(ss[1])))
-print(len(set(ss[9])))
+print(len(set(ss[1]))) # part1
+print(len(set(ss[9]))) # part2
