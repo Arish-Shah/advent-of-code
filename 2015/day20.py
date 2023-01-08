aa177fc60b8743e540f8c4b6e98ed118396bdb0d
Arish-Shah <arish.r.shah@gmail.com>
2023-01-08 21:14:59 +0100

    add day20 part 1

--- 2015/day20.py
+++ 2015/day20.py
@@ -1,3 +1,17 @@
 puzzle = 33100000
+house = 1
 
+def get_divisors(n):
+    for i in range(1, int(n / 2) + 1):
+        if n % i == 0:
+            yield i
+    yield n
 
+while True:
+    gifts = sum(get_divisors(house))
+    print(house, gifts)
+    if gifts >= int(puzzle / 10):
+        break
+    house += 1
+
+print(house)
